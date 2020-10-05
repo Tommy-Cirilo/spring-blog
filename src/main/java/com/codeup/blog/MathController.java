1c0bb4ce2165f14c208f4855292025c2238ce2c0
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-10-05 11:35:24 -0500

    finished all of the math controller class with addition, subtraction, multiplication, and division

--- src/main/java/com/codeup/blog/MathController.java
+++ src/main/java/com/codeup/blog/MathController.java
@@ -1,4 +1,47 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+    @RequestMapping(path = "/add/{number}/and/{number2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number, @PathVariable int number2) {
+        return number + " plus the second number " + number2 + " is " + (number + number2);
+    }
+
+    @Controller
+    public class SubtractController {
+        @RequestMapping(path = "/subtract/{number}/from/{number2}", method = RequestMethod.GET)
+        @ResponseBody
+        public String addOne(@PathVariable int number, @PathVariable int number2) {
+            return number + " Subtracted from  " + number2 + " is " + (number - number2);
+        }
+    }
+
+    @Controller
+    public class MultiplyController {
+        @RequestMapping(path = "/multiply/{number}/and/{number2}", method = RequestMethod.GET)
+        @ResponseBody
+        public String addOne(@PathVariable int number, @PathVariable int number2) {
+            return number + " Multiplied by  " + number2 + " is " + (number * number2);
+        }
+    }
+
+    @Controller
+    public class DivideController {
+        @RequestMapping(path = "/divide/{number}/by/{number2}", method = RequestMethod.GET)
+        @ResponseBody
+        public String addOne(@PathVariable int number, @PathVariable int number2) {
+            return number + " Divided by  " + number2 + " is " + (number / number2);
+        }
+    }
+
+
+
+
 }
