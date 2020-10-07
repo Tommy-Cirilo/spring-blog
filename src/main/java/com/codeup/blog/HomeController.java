f26ee57f19025d78af7d68c5121485a7ef06f7ff
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-10-07 10:08:01 -0500

    adding more controllers

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -1,15 +1,33 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    @ResponseBody
-    public String Home() {
-        return "This is the Landing Page!";
+    public String welcome() {
+        return "home";
+    }
+
+    @GetMapping("/roll-dice")
+    public String showDiceForm() {
+        return "dice";
+    }
+
+    @PostMapping("/roll-dice")
+    public String showDiceResult(@RequestParam(name = "number") int number, Model model) {
+        int numberRolled = (int) ((Math.random() * 6) + 1);
+
+        String message = "you selected " + number + " and the number rolled was " + numberRolled + ".";
+        if (number == numberRolled) {
+            message += " You Won!";
+        } else {
+            message += " Oh well... try again!";
+        }
+        model.addAttribute("message", message);
+        return "dice";
     }
 }
