0b8f1478ca90be1e77670b76d84c77e070200267
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-10-14 11:11:07 -0500

    completed the ad and user controller

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,44 +1,99 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Ad;
+import com.codeup.blog.models.AdCategory;
 import com.codeup.blog.models.Post;
+import com.codeup.blog.repository.PostRepository;
+import com.codeup.blog.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
-    //This first request mapping gets the all of the return strings from the path /posts
+    private final PostRepository postRepo;
+    private final UserRepository userRepo;
+
+    public PostController(PostRepository postRepo, UserRepository userRepo) {
+        this.postRepo = postRepo;
+        this.userRepo = userRepo;
+    }
+
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
     public String showAllPosts(Model model) {
-        List<Post> postList = new ArrayList<>();
-        postList.add(new Post(0, "First Post", "This is the first Post"));
-        postList.add(new Post(0, "Second Post", "This is the second Post"));
-        model.addAttribute("posts", postList);
+        model.addAttribute("posts", postRepo.findAll());
         return "posts/index";
     }
 
-    //This get mapping gets an individual posts id and returns a single post
     @GetMapping("/posts/{id}")
     public String showOnePost(@PathVariable long id, Model model) {
-        Post post = new Post(0, "Single Post", "This is the body for the single post");
+        Post post = postRepo.getOne(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
 
-    //This request mapping views the form and gets the data for creating a post
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
     public String createPostForm() {
-        return "Tell me about your day!";
+        return "posts/create";
     }
 
-    //This request mapping actually post and sends the created post from the form to the database
     @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String sendingCreatedPostToDB() {
-        return "Sending your diary page to be seen by everyone. :)";
+    public String createPost(@RequestParam(name = "title") String title,
+                             @RequestParam(name = "body") String body,
+                             Model model) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setBody(body);
+        postRepo.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
+
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id, Model model) {
+        Post post = postRepo.getOne(id);
+        if (post != null) {
+            postRepo.delete(post);
+        }
+        return "redirect:/posts";
+    }
+
+
+    @GetMapping("/posts/edit/{id}")
+    public String showEditPost(@PathVariable long id, Model model) {
+        Post post = postRepo.getOne(id);
+        if (post == null) {
+            return "redirect:/posts/index";
+        }
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+
+    @PostMapping("/posts/edit")
+    public String updatePost(@RequestParam(name = "id") long id,
+                             @RequestParam(name = "title") String title,
+                             @RequestParam(name = "body") String body,
+                             Model model) {
+        Post post = postRepo.getOne(id);
+        if (post == null) {
+            return "redirect:/posts/index";
+        }
+        post.setTitle(title);
+        post.setBody(body);
+        postRepo.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("posts/hardcoded/create")
+    public String createHardcodedAd() {
+        Post post = new Post();
+        post.setTitle("Love letter to my beloved");
+        post.setBody("Sometimes Furby... I think you are the only one who understands me... Thanks for being there for me.");
+        post.setUser(userRepo.getOne(1L));
+        postRepo.save(post);
+        return "redirect:/posts";
     }
 }
